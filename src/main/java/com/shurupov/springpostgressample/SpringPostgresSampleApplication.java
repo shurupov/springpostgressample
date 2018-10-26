2aeb7c38c95afd19caf7d6d7a6959dfc2b3e2bba
Evgeny Shurupov <bolteg86@ya.ru>
2018-10-26 20:02:30 +0400

    Initial commit

---
+++ src/main/java/com/shurupov/springpostgressample/SpringPostgresSampleApplication.java
@@ -0,0 +1,12 @@
+package com.shurupov.springpostgressample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresSampleApplication.class, args);
+	}
+}
